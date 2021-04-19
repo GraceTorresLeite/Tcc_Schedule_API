677541f7b18c214dbb9c0a4358da92db7909a394
GraceTorresLeite <grace.torresleite@gmail.com>
2021-04-18 22:58:00 -0300

    incluindo classes

---
+++ Tcc.Api/Hypermedia/Abstract/ISupportHyperMedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Tcc.Api.Hypermedia.Abstract
+{
+    public interface ISupportHyperMedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
---
+++ Tcc.Api/Hypermedia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Tcc.Api.Hypermedia.Abstract;
+
+namespace Tcc.Api.Hypermedia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
